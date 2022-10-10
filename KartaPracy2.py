59ad21a3ea1670071411b854a08257ab40fea7ba
1b32IW <114583419+WaltuhWyl@users.noreply.github.com>
2022-10-10 12:20:52 +0200

    Create KartaPracy2.py
    dodano kartę parcy 2

---
+++ KartaPracy2.py
@@ -0,0 +1,38 @@
+#Zad 1
+
+# a = int(input())
+
+# if a % 3 ==0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#Zad 2
+
+# a = int(input())
+
+# if a >=100 and a < 1000 and a % 17 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#Zad 3
+
+# a = int(input())
+
+# if a >=18:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#Zad 4
+
+waga = int(input())
+LIMIT = 20
+if waga <= LIMIT:
+  print("Jedź ze spokojem")
+else:
+  print("NIE")
+
+#Zad 5 
+
