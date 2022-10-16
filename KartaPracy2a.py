bccdaeccb5c361428d3f89a0ff13906f2204d981
1b32IW <114583419+WaltuhWyl@users.noreply.github.com>
2022-10-16 18:47:56 +0200

    Dodaję KartaPracy2a

---
+++ KartaPracy2a.py
@@ -0,0 +1,84 @@
+# Zad 1
+
+# a = int(input())
+# b = int(input())
+
+# if (a + b) % 2 ==0:
+#   print("Tak")
+# else:
+#   print("Nie")
+
+# Zad 2
+
+# a = int(input())
+# g = int(input())
+
+# if (a + g)/2 > (a * g)**1/2:
+#   print("Tak")
+# else:
+#   print("Nie")
+
+# Zad 3
+
+# k = int(input())
+# l = int(input())
+# m = int(input())
+
+# if k !=l and k !=m and m !=l:
+#   print("Nie")
+
+# if k ==l and k !=m and m !=l:
+#   print("Tak", k)
+
+# if k !=l and k ==m and m !=l:
+#   print("Tak", m)
+
+# if k !=l and k !=m and m ==l:
+#   print("Tak", l)       
+
+# if k ==l and k ==m and m ==k:
+#   print("Tak", l)
+
+# Zad 4
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+
+# if a<b and a<c and a<d:
+#   print(a)
+# if b<a and b<c and b<d:
+#   print(b)
+# if c<a and c<b and c<d:
+#   print(c)
+# if d<a and d<b and d<c:
+#   print(d)
+# if a ==b and b ==c and c ==d:
+#   print(a)
+  
+# Zad 5
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# if a<b + c and b<a + c and c<a + b:
+#   print("Tak")
+# else:
+#   print("Nie")
+
+# Zad 6
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+  
+# if (a**2 + b**2)>c**2 and (a**2 + c**2)>b**2 and (c**2 + b**2)>a**2:
+#   print("Ostrokątny")
+
+# if (a**2 + b**2) ==c**2 or (a**2 + c**2) ==b**2 or (c**2 + b**2) ==a**2:
+#   print("Prostokątny")
+
+# if (a**2 + b**2)<c**2 or (a**2 + c**2)<b**2 or (c**2 + b**2)<a**2:
+#   print("Rozwartokątny")
