95503caeae8bac69a88de0dabed9b7dae9511106
1b32IW <114583419+WaltuhWyl@users.noreply.github.com>
2022-11-01 20:47:41 +0100

    Dodaję niepełną KartaPracy3b.py

---
+++ KartaPracy3b.py
@@ -0,0 +1,57 @@
+#Zad 1 +
+
+# for (i) in range(1, 31):
+#   print(i, end=" ")
+
+#Zad 2 +
+
+# for (i) in range(1, 10, 2):
+#  print(i**2)
+
+#Zad 3 +
+
+# for (i) in range(1137, 10000, 379):
+#   print(i)
+
+#Zad 4 +
+
+# for (i) in range(100, 1000):
+#   if (i%5 ==0 and i%6 ==0) or (i%5 ==0 and i%6 ==0 and i%11 ==0):
+#     print(i)
+
+#Zad 5 +
+
+# n = int(input())
+# suma = 0
+# for i in range(n):
+#   liczby = int(input())
+#   suma = suma + liczby
+# print(suma)
+
+#Zad 6 +- "Nie jestem pewien co to liczby początkowe"
+
+# k = int(input())
+# suma = 0
+# for i in range(0, k, 2):
+#   suma = suma + i
+# print(suma)
+
+#Zad 7 +- "Nie jestem pewien co to liczby początkowe"
+
+# m = int(input())
+# suma = 0
+# for i in range(11, m, 2):
+#   suma = suma + i
+# print(suma)
+
+#Zad 8 -
+
+#Zad 9 +
+
+# n = int(input())
+# suma = 0
+# for i in range(21, n, 100):
+#   suma += i
+# print(suma)
+
+#Zad 10 -
