752afae0f86584c803651631922115ad100f33fa
1b32IW <114583419+WaltuhWyl@users.noreply.github.com>
2022-11-16 14:01:06 +0100

    Dodaję KartaPracy3a.py

---
+++ KartaPracy3a.py
@@ -0,0 +1,41 @@
+#Zad 1
+# n = int(input())
+# for i in range(n):
+#   print("*-|", end="")
+
+#Zad 2
+# n = int(input())
+# for i in range(1, n+1):
+#   print("*"*i, end="")
+#   if i%2 ==0:
+#     print("--", end= "")
+#   else:
+#     print("||", end= "")
+
+#Zad 3-
+
+# n = int(input())
+# for i in range(1, n+1):
+#   print("*", end="")
+#   if i%2 ==0:
+#     print("-", end="")
+#   else:
+#     print("|"*i, end="")
+
+#Zad 7
+
+# n = int(input())
+
+# for i in range(n):
+#   for j in range(n):
+#     print(f"({i},{j})",end="")
+# print()
+
+# n = int(input())
+# for i in range(n):
+#   for j in range(n):
+#     if i==0 or j==0 or j==n-1 or i==n-1 or (i==n//2 and j==n//2):
+#       print("*",end="")
+#     else:
+#       print("-",end="")
+#   print()
