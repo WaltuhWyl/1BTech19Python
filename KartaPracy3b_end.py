9ab74ab68d9ce969d751cde982fc5532806501ba
1b32IW <114583419+WaltuhWyl@users.noreply.github.com>
2022-12-06 19:35:29 +0100

    Dodaję KartaPracy3b_end.py

---
+++ KartaPracy3b_end.py
@@ -0,0 +1,63 @@
+#Zad 1
+
+# n = int(input())
+# for i in range(n):
+#   print("*-|", end="")
+
+#Zad 1
+
+# for i in range(1,31):
+#   print(i, end=" ")
+
+#Zad 2
+
+# for i in range(1,10,2):
+#   print(i**2)
+
+#Zad 3
+
+# for i in range(1137,10000, 379):
+#   print(i)
+
+#Zad 4
+
+# for i in range(100, 1000):
+#   if i%5==0 and i%6==0 or (i%5==0 and i%6==0 and i%11==0):
+#     print(i)
+
+#Zad 5
+
+# n = int(input())
+# suma = 0
+# for i in range(n):
+#   a = int(input())
+#   suma += a
+# print(suma)
+
+#Zad 6
+
+# n = int(input())
+# suma = 0
+# for i in range(0, n*2+2, 2):
+#   suma += i
+# print(suma)
+
+#Zad 7
+
+# m = int(input())
+# suma = 0
+# for i in range(11, m*2+11 ,2):
+#   suma += i
+# print(suma)
+
+#Zad 8
+
+#Zad 9
+
+# n = int(input())
+# suma = 0
+# for i in range(n):
+#   suma += (100*i)+21
+# print(suma)
+
+#Zad 10
